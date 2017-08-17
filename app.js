c1512e9fff72180d42bb2d176973dbf1bb8efb2b
unknown <tech1arrb@gmail.com>
2017-08-18 01:21:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
+var cors = require('cors');
+var path = require('path');
+
+var app = express();
+
+const route = require('./routes/route');
+
+mongoose.connect('mongodb://blog:arb1tech@ds149373.mlab.com:49373/blog');
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database');
+})
+mongoose.connection.on('error', (err) => {
+    if (err) {
+        console.log('Error in databse connection:' + err);
+    }
+    console.log('Connected to database');
+})
+const port = process.env.PORT || 3000;
+
+
+app.use(cors());
+
+app.use(bodyparser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', route);
+
+app.get('/', (req, res) => {
+    res.send('Test');
+})
+
+app.listen(port, () => {
+    console.log('server started at port', +port);
+});
