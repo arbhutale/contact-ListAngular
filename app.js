1a6d777c6ac7e874ed87a0886226dcef4881db04
unknown <tech1arrb@gmail.com>
2017-08-18 01:49:03 +0530

    first commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ mongoose.connection.on('error', (err) => {
     }
     console.log('Connected to database');
 })
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 
 app.use(cors());
